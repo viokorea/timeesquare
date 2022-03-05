6d0ad99b962410e44e484a36ab7efba01ba8e1cd
yung <ly8844@naver.com>
2022-03-05 13:29:00 +0900

    커맨드 리스트 만들예정이고 리스트 데이터에 들어갈 레이아웃 만들어봄

--- app/src/main/java/com/esqtwo/timesquare/MainActivity.java
+++ app/src/main/java/com/esqtwo/timesquare/MainActivity.java
@@ -3,12 +3,24 @@ package com.esqtwo.timesquare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview1;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
---
+++ app/src/main/java/com/esqtwo/timesquare/listviewitem.java
@@ -0,0 +1,8 @@
+package com.esqtwo.timesquare;
+
+import android.graphics.drawable.Drawable;
+
+public class listviewitem {
+    private Drawable capsule;
+
+}
