a3c5e6cec6b77b0807d6accfae2bfbd9c9d7d534
yung <ly8844@naver.com>
2022-03-05 13:33:59 +0900

    커맨드 리스트 만들예정이고 리스트 데이터에 들어갈 레이아웃 만들어봄

--- app/src/main/java/com/esqtwo/timesquare/listviewitem.java
+++ app/src/main/java/com/esqtwo/timesquare/listviewitem.java
@@ -4,5 +4,9 @@ import android.graphics.drawable.Drawable;
 
 public class listviewitem {
     private Drawable capsule;
+    private String et_1;
 
+    public void setCapsule(Drawable capsule){
+
+    }
 }
